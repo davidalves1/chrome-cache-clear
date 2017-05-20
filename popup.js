b898251f61d1ac171224fa1bb730573438694ed2
davidalves1 <alves.david@outlook.com>
2017-05-19 22:46:26 -0300

    Correções e atualizações

--- popup.js
+++ popup.js
@@ -1,24 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	var button = document.querySelector('#clear_cache');
+	var button = document.querySelector('#btnClear');
 
 	var clearChromeCache= function() {
-		chrome.browsingData.removeCache({}, function() {
-			var message = document.querySelector('#message');
-
-			message.innerHTML = '<p style="color: #1b5e20; text-align: center;">Clear chache!</p>';
-		});
-
 		chrome.browsingData.remove(
 			{
-				"since": 0
+				"since": 0 // Define que irá excluir desde o início
 			}, {
 				"appcache": true,
 			    "cache": true
 			}, function() {
 				var message = document.querySelector('#message');
 
-				message.innerHTML = '<p style="color: #1b5e20; text-align: center;">Clear cookies!</p>';
+				message.innerHTML = '<span>-- Cache limpo --</span>';
+
+				setTimeout(function() { message.innerHTML = ''}, 2000);
 			});
 	}
 
