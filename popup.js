c9944446076f4e16b405d9ce9b78d7cd53dbc61b
davidalves1 <alves.david@outlook.com>
2017-05-20 16:15:54 -0300

    Separar arquivo de css e incluir opções para limpar

--- popup.js
+++ popup.js
@@ -2,22 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	var button = document.querySelector('#btnClear');
 
-	var clearChromeCache= function() {
+	button.addEventListener('click', clearChromeCache);
+
+	function clearChromeCache() {
+		var message = document.querySelector('#message');
+
+		message.innerHTML = '<span>-- Limpando... --</span>';
+		
 		chrome.browsingData.remove(
 			{
-				"since": 0 // Define que irá excluir desde o início
+				'since': 0 // Define que irá excluir desde o início
 			}, {
-				"appcache": true,
-			    "cache": true
+				'appcache': document.querySelector('#cache').checked,
+			    'cache': document.querySelector('#appCache').checked,
+			    'cookies': document.querySelector('#cookies').checked
 			}, function() {
-				var message = document.querySelector('#message');
-
-				message.innerHTML = '<span>-- Cache limpo --</span>';
+				message.innerHTML = '<span>-- Concluído --</span>';
 
 				setTimeout(function() { message.innerHTML = ''}, 2000);
 			});
 	}
-
-	document.addEventListener('click', clearChromeCache);
-
 }, false);
